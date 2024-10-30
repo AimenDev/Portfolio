ac2c503cf857d7fb95c6f1defb7cdd397612cec3
AimenDev <aimendev4@gmail.com>
2024-10-30 20:17:22 +0100

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Github, SquareArrowOutUpRight } from 'lucide-react';
 
-const Card = ({ image, title, description, languages }) => {
+const Card = ({ image, title, description, languages, githubLink, liveDemoLink }) => {
   return (
     <div className="bg-transparent border border-gray-700 shadow-md dark:shadow-lg rounded-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:-translate-y-1 hover:shadow-xl hover:border-emerald-400">
       <img src={image} alt={title} className="w-full h-48 object-cover" />
       <div className="p-4">
-        <h3 className="text-lg font-bold mb-2 text-white font-sans ">{title}</h3>
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="text-lg font-bold text-white dark:text-neutral-900 font-sans">{title}</h3>
+          <div className="flex gap-2">
+            {githubLink && (
+              <a href={githubLink} target="_blank" rel="noopener noreferrer" className="text-gray-300 dark:text-neutral-900  hover:text-white">
+                <Github className="w-5 h-5" />
+              </a>
+            )}
+            {liveDemoLink && (
+              <a href={liveDemoLink} target="_blank" rel="noopener noreferrer" className="text-gray-300 dark:text-neutral-900 hover:text-white">
+                <SquareArrowOutUpRight className="w-5 h-5" />
+              </a>
+            )}
+          </div>
+        </div>
         <p className="text-neutral-400 dark:text-neutral-600 mb-4">{description}</p>
         <div className="flex flex-wrap gap-2 font-fira">
           {languages.map((lang, index) => (
-            <span key={index} className="bg-gray-200 dark:bg-gray-300 text-gray-800  text-sm font-medium px-2 py-1 rounded">
+            <span key={index} className="bg-gray-200 dark:bg-gray-300 text-gray-800 text-sm font-medium px-2 py-1 rounded">
               {lang}
             </span>
           ))}
--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,45 +1,64 @@
 import React from 'react';
 import Card from '../components/Card';
+import { Github, SquareArrowOutUpRight } from 'lucide-react';
 
 const projects = [
   {
-    image: 'path-to-image1.jpg',
-    title: 'Project One',
-    description: 'This is a brief description of project one.',
-    languages: ['React', 'Tailwind CSS', 'JavaScript'],
+    image: "./E-commerce.png",
+    title: "E-Commerce Store",
+    description: "A full-featured e-commerce platform built using the MERN stack",
+    languages: ["MongoDB", "Express.js", "React", "Node.js", "Tailwind CSS"],
+    githubLink: "https://github.com/AimenDev/E-commerce",
+    liveDemoLink: "https://e-commerce-qgeg.onrender.com"
   },
   {
-    image: 'path-to-image2.jpg',
-    title: 'Project Two',
-    description: 'This is a brief description of project two.',
+    image: './Inspire-Ai.png',
+    title: 'Inspire-Ai | Prompt Generator',
+    description: 'Inspire-Ai is an AI-powered prompt generation web application built with Next.js',
     languages: ['Next.js', 'Tailwind CSS', 'Node.js'],
+    githubLink: "https://github.com/AimenDev/InspireAI",
+    liveDemoLink: "https://inspireai-one.vercel.app"
   },
   {
-    image: 'path-to-image3.jpg',
-    title: 'Project Three',
-    description: 'This is a brief description of project three.',
-    languages: ['Next.js', 'Tailwind CSS', 'Node.js'],
+    image: './Chat-App.png',
+    title: 'Chat App',
+    description: 'A real-time chat application built using the MERN stack',
+    languages: ["MongoDB", "Express.js", "React", "Tailwind CSS", "WebSockets"],
+    githubLink: "https://github.com/AimenDev/Chat-App",
+    liveDemoLink: "https://chat-app-5t7r.onrender.com"
   },
   {
-    image: 'path-to-image4.jpg',
-    title: 'Project Four',
-    description: 'This is a brief description of project four.',
-    languages: ['Next.js', 'Tailwind CSS', 'Node.js'],
+    image: './Xyber.png',
+    title: 'Saas Landing Page',
+    description: 'A sleek and modern SaaS landing page designed to showcase services and drive conversions',
+    languages: ['React', 'Tailwind CSS'],
+    githubLink: "https://github.com/AimenDev/Xyber-Ai-landing-page",
+    liveDemoLink: ""
   },
   {
-    image: 'path-to-image5.jpg',
-    title: 'Project Five',
-    description: 'This is a brief description of project five.',
-    languages: ['Next.js', 'Tailwind CSS', 'Node.js'],
-  }
+    image: './Recipes.png',
+    title: 'Recipes Finder',
+    description: 'Recipes Finder is a web app designed to help users discover and explore recipes based on their preferences',
+    languages: ['React', 'Tailwind CSS'],
+    githubLink: "https://github.com/AimenDev/Recipe-Finder-App",
+    liveDemoLink: ""
+  },
+  {
+    image: './VR.png',
+    title: 'VR Landing Page',
+    description: 'A visually immersive landing page built for a VR product or service',
+    languages: ['React', 'Tailwind CSS'],
+    githubLink: "https://github.com/AimenDev/virtual-landing-page",
+    liveDemoLink: "https://virtualcode.vercel.app/"
+  },
 ];
 
 const Main = () => {
   return (
-    <section className="py-16 xl:px-16 bg-transparent border-t border-white dark:border-neutral-700 " id='projects'>
+    <section className="py-16 xl:px-16 bg-transparent border-t border-white dark:border-neutral-700" id='projects'>
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold font-roboto text-center mb-20 text-white dark:text-neutral-900">Things I’ve made trying to put my dent in the universe.</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-4  ">
+        <h2 className="text-3xl font-bold font-roboto text-center mb-20 text-white dark:text-neutral-900">Things I’ve made</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-4">
           {projects.map((project, index) => (
             <Card
               key={index}
@@ -47,6 +66,8 @@ const Main = () => {
               title={project.title}
               description={project.description}
               languages={project.languages}
+              githubLink={project.githubLink}
+              liveDemoLink={project.liveDemoLink}
             />
           ))}
         </div>
